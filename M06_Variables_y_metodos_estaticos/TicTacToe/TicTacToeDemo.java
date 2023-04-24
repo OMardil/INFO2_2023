45e41cdaebd84050f1c8950cf335f4993c3de7bb
Omar Acosta <omardil.acosta@gmail.com>
2023-04-23 21:50:42 -0600

    Upate some files

---
+++ M06_Variables_y_metodos_estaticos/TicTacToe/TicTacToeDemo.java
@@ -0,0 +1,12 @@
+package games;
+
+public class TicTacToeDemo {
+
+	public static void main(String[] args) {
+
+		TicTacToe game = new TicTacToe("Omar","Eduardo");
+		game.play();
+		
+	}
+
+}
--- M07_Variables_y_metodos_estaticos/src/src/src/CircleDemo.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class CircleDemo {
-
-	public static void main(String[] args) {
-		System.out.println("The value of PI is: "+ Circle.PI);
-	}
-}
